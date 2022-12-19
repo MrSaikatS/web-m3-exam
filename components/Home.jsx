29303a09a57fd522cc594a3b33d8140d9146a3e8
Saikat Sardar <saikatsardar5@gmail.com>
2022-12-19 15:37:49 +0530

    v 0.3.0

---
+++ components/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+	return (
+		<>
+			<div className='grid place-items-center w-screen h-screen'>
+				<div className='text-4xl font-bold'>Hello !!</div>
+			</div>
+		</>
+	);
+};
+
+export default Home;
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Home from "../components/Home";
+
+const index = () => {
+	return (
+		<>
+			<Home />
+		</>
+	);
+};
+
+export default index;
